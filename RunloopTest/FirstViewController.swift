595b5f64fbdbed0c7b41f3ca66763db293a7185d
Artus <artus91@ukr.net>
2018-07-25 11:44:32 +0300

    Repeating timer created and introduced in project

--- RunloopTest/FirstViewController.swift
+++ RunloopTest/FirstViewController.swift
@@ -9,27 +9,54 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var dateAndTime: UILabel!
+    @IBOutlet weak var selectedFeed: UILabel!
+    private var isAppeared = false
+    private lazy var timer: RepeatingTimer = {
+        return RepeatingTimer(timeInterval: 1)
+    }()
+    
+    private var currentTime : String {
+        let dateFormatter : DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MMM-dd HH:mm:ss"
+        let dateString = dateFormatter.string(from: Date())
+        return dateString
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        timer.eventHandler = {
+            DispatchQueue.main.async {
+                self.dateAndTime.text = self.currentTime
+            }
+        }
     }
-    */
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print ("willAppear \(isViewLoaded)")
+        self.dateAndTime.text = self.currentTime
+        timer.resume()
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        timer.suspend()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
